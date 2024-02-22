6b2f7b00feab001f49992678bc01ef7235731e59
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2024-02-22 17:31:44 +0530

    wrote schema for mongodb. Added some test cases and class in dev for rec

--- main.py
+++ main.py
@@ -16,13 +16,16 @@ app = FastAPI()
 # app.include_router(product_routes.router)
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+	CORSMiddleware,
+	allow_origins=["http://localhost:3000"],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
 )
+
 
 @app.get("/hello")
 def index():
-    return {"message": "Hello World"}
+	return {"message": "Hello World"}
+
+
