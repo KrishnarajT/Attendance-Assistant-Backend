f6ae96bb52c78fa4ba540193cf93dd2bf7182f4e
Parth Zarekar <90327206+Parth4123@users.noreply.github.com>
2024-02-28 00:50:41 +0530

    added add lecture, get lecture by id, get_all_lectures

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
 
+
 app = FastAPI()
 
 # Include routers here
-from routers import client_uploads, test_route, face_rec, college, subjects, students, teachers, classes, panels
+from routers import client_uploads, test_route, face_rec, college, subjects, students, teachers, panels, lectures
 
 app.include_router(client_uploads.router)
 app.include_router(students.router)
@@ -13,6 +14,6 @@ app.include_router(panels.router)
 app.include_router(college.router)
 app.include_router(subjects.router)
 app.include_router(teachers.router)
-app.include_router(classes.router)
+app.include_router(lectures.router)
 app.include_router(test_route.router)
 
--- app/routers/face_rec.py
+++ app/routers/face_rec.py
@@ -8,6 +8,8 @@ from fastapi import APIRouter
 # import face rec stuff
 from facial_recognition.FaceRec import FaceRec  # main class
 
+# import db stuff
+from services.assistanceMongoDB import MongoService
 # import models
 from models.ClientUploadModels import AttendanceModel
 from models.StudentModels import EncodingModel
@@ -23,7 +25,28 @@ def index():
 	return "Hello, World!"
 
 
-@router.get("/get_attendance", status_code=200, summary="Get attendance")
+@router.post("/add_attendance", status_code=200, summary="add attendace")
 def get_attendance(class_att: AttendanceModel):
+	"""
+	Add attendance to the database.
+
+	:param class_att: The attendance model.
+
+	:return: The attendance model.
+	"""
+	# get the lecture images and student encodings from the database
+
+	#get all lecture images in between the start and end time
+
+	mongo_obj = MongoService()
+	
+	lecture_images = mongo_obj.get_lecture_images_between_time(class_att.start_time, class_att.end_time)
+
+	# get all encodings of students in the panel
+	student_encodings = mongo_obj.get_student_encodings_from_panel_id(class_att.panel_id)
+	# instantiate the face rec object
+	face_rec_obj = FaceRec(lecture_images,student_encodings,panel_id,student_ids)
+
+	# add new row to classes collection
 	pass
 	
